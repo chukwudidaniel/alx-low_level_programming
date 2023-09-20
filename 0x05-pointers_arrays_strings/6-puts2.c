d27b982323ffbcf95794c0f448506a50ca9a93a6
chukwudidaniel <chukwudiobiegbu2@gmail.com>
2023-09-19 23:32:03 -0700

    m

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,20 +10,20 @@ void puts2(char *str)
 	int longi = 0;
 	int t = 0;
 	char *y = str;
-	int 0;
+	int o;
 	
 	while (*y != '\0')
 	{
 		y++;
 		longi++;
 	}
-	t = loni - i;
+	t = longi - 1;
 	for (o = 0 ; o <= t ; o++)
 	{
 		if (o % 2 == 0)
-	{
-		_putchar(str[o]);
-	}
+		{
+			_putchar(str[o]);
+		}
 	}
 	_putchar('\n');
 }
