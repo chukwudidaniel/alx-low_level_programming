2263171d17ef9b921e0ff0c5ac992acd92a6c546
chukwudidaniel <chukwudiobiegbu2@gmail.com>
2023-09-21 03:47:37 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,3 +1,31 @@
 #include "main.h"
 /**
- *
+ * rot13 - Encodes a string using ROT13.
+ * @str: The input string to be encoded.
+ * Return: A pointer to the resulting ROT13-encoded string.
+ */
+char *rot13(char *str)
+{
+	char *p = str; /* p = input */
+	char *t = str; /* t = output */
+	char rot13_table[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*p)
+	{
+		char c = *p;
+
+		if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		{
+			*t = rot13_table[c - ((c >= 'a' && c <= 'z') ? 'a' : 'A')];
+		}
+		else
+		{
+			*t = c;
+		}
+		p++;
+		t++;
+	}
+	*t = '\0';
+
+	return (str);
+}
