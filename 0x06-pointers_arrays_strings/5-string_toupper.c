006f1f34c3f7be39b572d46c6ba92f7b498d4a8f
chukwudidaniel <chukwudiobiegbu2@gmail.com>
2023-09-21 00:41:04 -0700

    task 5 Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,3 +1,19 @@
 #include "main.h"
 /**
- *
+ * *string_toupper - converts a strng to upper case
+ * @str: string input to be converted
+ * Return: modified string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
