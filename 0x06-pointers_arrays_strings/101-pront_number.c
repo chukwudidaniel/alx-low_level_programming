860923f8600df5490dd98e751edfeee18075a5c1
chukwudidaniel <chukwudiobiegbu2@gmail.com>
2023-09-21 04:07:55 -0700

    task 101 advanced

--- 0x06-pointers_arrays_strings/101-pront_number.c
+++ 0x06-pointers_arrays_strings/101-pront_number.c
@@ -1,3 +1,20 @@
-#include "main.c"
+#include "main.h"
 /**
- *
+ * print_number - prints integer
+ * @n: input integer parameter
+ */
+void print_number(int n)
+{
+	unsighned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(1 % 10 + '0');
+}
