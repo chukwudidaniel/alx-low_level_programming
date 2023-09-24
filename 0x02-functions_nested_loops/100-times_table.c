9046abdf62a1efea99f68a4ef618f81fa40e8262
chukwudidaniel <chukwudiobiegbu2@gmail.com>
2023-09-24 10:48:16 -0700

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,21 +1,42 @@
-#include "main.h"
-
+#include <stdio.h>
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_times_table - prints the time table for n from 0
+ * @n: the number of interest
+ * Return: always 0
  */
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+	{
+		return; /* Return early if n is out of range */
+	}
 
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= n; j++)
+		{
+			int result = i * j;
+
+			if (j == 0)
+			{
+		/* Print with leading space for single-digit numbers */
+				printf("%2d", result);
+			}
+			else
+			{
+		/* Print with leading space for single-digit numbers */
+				printf(", %2d", result);
+			}
+		}
+		printf("\n"); /* Move to the next row */
+	}
+}
 
 int main(void)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
+	int n = 12; /* Change n to the desired value (e.g., 12) */
+
+	print_times_table(n);
 	return (0);
 }
