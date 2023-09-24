e8db3d3e8aa1a3d76acd925ac3b5e2583e2e2fce
chukwudidaniel <chukwudiobiegbu2@gmail.com>
2023-09-24 11:01:15 -0700

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,11 +5,12 @@
  * @n: the number of interest
  * Return: always 0
  */
+
 void print_times_table(int n)
 {
 	if (n < 0 || n > 15)
 	{
-		return; /* Return early if n is out of range */
+		return;
 	}
 
 	for (int i = 0; i <= n; i++)
@@ -20,22 +21,24 @@ void print_times_table(int n)
 
 			if (j == 0)
 			{
-		/* Print with leading space for single-digit numbers */
 				printf("%2d", result);
 			}
 			else
 			{
-		/* Print with leading space for single-digit numbers */
 				printf(", %2d", result);
 			}
 		}
-		printf("\n"); /* Move to the next row */
+		printf("\n");
 	}
 }
 
+/**
+ * main - Entry point of the program
+ * Return: 0 on success
+ */
 int main(void)
 {
-	int n = 12; /* Change n to the desired value (e.g., 12) */
+	int n = 12;
 
 	print_times_table(n);
 	return (0);
